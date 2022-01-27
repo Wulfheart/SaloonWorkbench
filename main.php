46567221e26b83b6f82295d0742e06ef6264e55b
Alex Wulf <alex.f.wulf@gmail.com>
2022-01-27 16:46:58 +0100

    WIP

---
+++ main.php
@@ -0,0 +1,39 @@
+<?php
+
+include './vendor/autoload.php';
+
+class Connector extends \Sammyjo20\Saloon\Http\SaloonConnector
+{
+    public function defineBaseUrl(): string
+    {
+        return 'https://httpbin.org';
+    }
+}
+
+class Response extends \Sammyjo20\Saloon\Http\SaloonResponse
+{
+    public function toDTO(): int
+    {
+        return $this->status();
+    }
+}
+
+class GetRequest extends \Sammyjo20\Saloon\Http\SaloonRequest
+{
+    protected ?string $connector = Connector::class;
+
+    protected ?string $method = \Sammyjo20\Saloon\Constants\Saloon::GET;
+
+    protected ?string $response = Response::class;
+
+    public function defineEndpoint(): string
+    {
+        return '/get';
+    }
+}
+
+$req = new GetRequest();
+
+echo $req->send()->toDTO();
+
+echo "\n\n";
